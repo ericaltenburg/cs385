a0cff0f9665593510d9f3483c0f4d339f03ba12c
Eric Altenburg <ealtenbu@stevens.edu>
2019-02-13 11:00:21 -0500

    Add files via upload

---
+++ CS385Test/test.cpp
@@ -0,0 +1,39 @@
+/*
+ * Name			: test.cpp
+ * Author		: Eric Altenburg
+ * Date			: Jan 28, 2019
+ * Description	:
+ * Pledge		: I pledge my honor that I have abided by the Stevens Honor System.
+ */
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int max (int m, int n) {
+	return m > n? m : n;
+}
+
+int main(int argc, char *argv[]) {
+	int m, n;
+	istringstream iss;
+
+	if (argc != 3) {
+		cerr << "Usage: " << argv[0] << " <integer m> <integer n>"  << endl;
+		return 1;
+	}
+	iss.str(argv[1]);
+	if (!(iss >> m )) {
+		cerr << "Error: The first argument is not a valid integer." << endl;
+		return 1;
+	}
+	iss.clear();
+	iss.str(argv[2]);
+	if (!(iss >> n )) {
+		cerr << "Error: The second argument is not a valid integer." << endl;
+		return 1;
+	}
+	cout << "max(" << m << ", " << n << ") = " << max(m,n) << endl;
+	return 0;
+}
